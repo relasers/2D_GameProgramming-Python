a77e899399e00df4ef0e44f41085857355327532
relasers <relasers@gmail.com>
2016-10-05 14:51:52 +0900

    Resource Upload

---
+++ [Project]ELEM/Scene_MainGame.py
@@ -0,0 +1,40 @@
+from pico2d import *
+import FrameWork
+from Ruby import *
+
+FPS = 60.0
+FRS_TIME = 1.0 / FPS
+
+name = "MainState"
+ruby = None
+
+def enter():
+    global ruby
+    open_canvas(1200, 800)
+    ruby = Ruby(600, 600)
+
+def exit():
+    close_canvas()
+
+
+def update():
+    ruby.update()
+    pass
+
+
+def draw():
+    clear_canvas()
+    ruby.draw()
+    update_canvas()
+
+
+def handle_events():
+    events = get_events()
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
