861f7ba7ff74123b1c48881a7eb0ef2b3b3cd541
KimByeongJin :: HoldimProvae <relasers@gmail.com>
2016-10-18 02:55:52 +0900

    Particle Update

--- [Project]ELEM/obj_Particle.py
+++ [Project]ELEM/obj_Particle.py
@@ -3,7 +3,12 @@ from pico2d import *
 
 class Particle:
     opacify = 1
+    MX_frame = 0
+    img_frame = 0
     Fade_out = False
     def __init__(self,fadeout,size,zoommode,zoomsize):
         self.Fade_out = fadeout
         pass
+    def isDestroy(self):
+        if self.img_frame == self.MX_frame:
+            return True
