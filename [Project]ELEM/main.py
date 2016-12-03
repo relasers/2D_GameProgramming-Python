5ed8cf94b070043e905e19e0450c4d339efcf7cc
KimByeongJin :: HoldimProvae <relasers@gmail.com>
2016-12-03 22:17:52 +0900

    Update

--- [Project]ELEM/main.py
+++ [Project]ELEM/main.py
@@ -3,13 +3,12 @@ import platform
 import os
 
 
-"""
+
 if platform.architecture() == '32bit' :
     os.environ["PYSDL2_DLL_PATH"] = "./SDL2/x86"
 else:
     os.environ["PYSDL2_DLL_PATH"] = "./SDL2/x64"
 
-"""
 
 
 from pico2d import *
